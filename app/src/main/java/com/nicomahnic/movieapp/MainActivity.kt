75579a4ee5452be925266674a6e1c64a563df5e8
Nicolás Mahnic <nmahnic@pdssa.com>
2021-03-17 01:23:53 -0300

    first commit

---
+++ app/src/main/java/com/nicomahnic/movieapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nicomahnic.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// API KEY
+// 44b2d7f8d7950335502b8c5d180b9ea0
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
